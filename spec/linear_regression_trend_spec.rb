125824f8cea01feeece1cfa6d9f3a8407ec57080
Gabriel Fortuna <gee.forr@gmail.com>
2015-08-05 15:11:23 +0200

    test: add tests that ensure correct trend calculation

--- spec/linear_regression_trend_spec.rb
+++ spec/linear_regression_trend_spec.rb
@@ -5,7 +5,46 @@ describe LinearRegressionTrend do
     expect(LinearRegressionTrend::VERSION).not_to be nil
   end
 
-  it 'does something useful' do
-    expect(false).to eq(true)
+  context "for simple and linear data sets" do
+    let(:known_ys) { [10, 14, 18, 22, 26] }
+    subject        { LinearRegressionTrend::Calculator.new(known_ys) }
+
+    it 'calculates the slope' do
+      expect(subject.slope).to eq 4
+    end
+
+    it "calculates the intercept" do
+      expect(subject.intercept).to eq 6
+    end
+  end
+
+  context "for simple and non-linear data sets" do
+    context "that have a descending trend" do
+      let(:known_ys) { [1, 2, 3, 6, 10, 11, 12, 12, 12, 12, 15, 18, 18, 16, 16, 15, 14, 11, 9, 9, 7, 4, 2, 1, 1] }
+      subject        { LinearRegressionTrend::Calculator.new(known_ys) }
+
+      it 'calculates the slope and intercept' do
+        expect(subject.slope).to     eq -0.06384615384615384
+        expect(subject.intercept).to eq 10.31
+      end
+
+      it "calculates a descending trend" do
+        expect(subject.trend.first).to be > subject.trend[-1]
+      end
+    end
+
+    context "that have an ascending trend" do
+      let(:known_ys) { [1, 2, 3, 6, 10, 11, 12, 12, 12, 12, 15, 18, 18, 16, 16, 15, 14, 11, 9, 9, 7, 4, 2, 1, 1].reverse }
+      subject        { LinearRegressionTrend::Calculator.new(known_ys) }
+
+      it 'calculates the slope and intercept' do
+        expect(subject.slope).to     eq 0.06384615384615384
+        expect(subject.intercept).to eq 8.65
+      end
+
+      it "calculates an ascending trend" do
+        expect(subject.trend.first).to be < subject.trend[-1]
+      end
+    end
   end
 end
